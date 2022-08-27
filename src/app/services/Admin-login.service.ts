76b2f1e3b1969ce2d300563105dd4f575166e964
Kau-stubh <110881687+Kau-stubh@users.noreply.github.com>
2022-08-27 22:22:58 +0530

    Create Admin-login.service.ts

---
+++ src/app/services/Admin-login.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminloginService {
+
+  private apiServer = "https://localhost:44378/api/admin/login";
+
+  httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  }),
+responseType:'text' as 'json'
+}
+constructor(private httpClient: HttpClient) { }
+login(data:any):Observable<any>
+{
+  return this.httpClient.post<any>(this.apiServer,data,this.httpOptions)
+}
+}
